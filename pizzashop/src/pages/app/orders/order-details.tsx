7b85d66bc73d95831e28cce68e104299f84227cb
Alois Sailer <alois_sailer@yahoo.com.br>
2024-01-10 23:59:36 -0300

    detalhes do pedido

---
+++ pizzashop/src/pages/app/orders/order-details.tsx
@@ -0,0 +1,96 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function OrderDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: 548r63e2154sdcx558s</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-s-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">Alois Sailer</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">
+                (21) 12345-6789
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                alois_sailer@yahoo.com.br
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Realizado</TableCell>
+              <TableCell className="flex justify-end">há 3 minutos</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza Portuguesa Família</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 49,90</TableCell>
+              <TableCell className="text-right">R$ 99,80</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza Mussarela Família</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 45,90</TableCell>
+              <TableCell className="text-right">R$ 45,90</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell className="text-right font-medium">
+                R$ 145,70
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  );
+}
