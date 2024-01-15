d21be7d71fcf779a4652d8f074c3a3fb3075b067
Alois Sailer <alois_sailer@yahoo.com.br>
2024-01-14 21:37:25 -0300

    produtos terminado

--- pizzashop/src/pages/app/dashboard/popular-products-chart.tsx
+++ pizzashop/src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,23 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import colors from "tailwindcss/colors";
 import { BarChart } from "lucide-react";
+import { ResponsiveContainer, Pie, PieChart, Cell } from "recharts/";
+import colors from "tailwindcss/colors";
 
-import {
-  ResponsiveContainer,
-  LineChart,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Line,
-  Tooltip,
-} from "recharts/";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const data = [
   { product: "Marguerita", amount: 40 },
@@ -25,6 +10,14 @@ const data = [
   { product: "Pepperoni", amount: 25 },
   { product: "Quatro Queijos", amount: 32 },
   { product: "Da Casa", amount: 20 },
+];
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
 ];
 export function PopularProductsChart() {
   return (
@@ -39,27 +32,58 @@ export function PopularProductsChart() {
       </CardHeader>
       <CardContent>
         <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })
-              }
-            />
-            <Line
-              type="linear"
-              dataKey="revenue"
-              strokeWidth={2}
-              stroke={colors.violet["400"]}
-            ></Line>
-          </LineChart>
+          <PieChart style={{ fontSize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180;
+                const radius = 12 + innerRadius + (outerRadius - innerRadius);
+                const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? "start" : "end"}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat("...")
+                      : data[index].product}{" "}
+                    ({value})
+                  </text>
+                );
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                );
+              })}
+            </Pie>
+          </PieChart>
         </ResponsiveContainer>
       </CardContent>
     </Card>
